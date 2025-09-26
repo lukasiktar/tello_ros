7097297d51e04ff83f99859c81c417561854c2fc
lukasiktar <luka.siktar11@gmail.com>
2025-09-26 15:29:23 +0200

    tello_ros package modification for additional functionalities.

--- tello_driver/include/tello_joy_node.hpp
+++ tello_driver/include/tello_joy_node.hpp
@@ -2,6 +2,7 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "tello_msgs/srv/tello_action.hpp"
+#include "std_srvs/srv/trigger.hpp"
 
 namespace tello_joy
 {
@@ -15,8 +16,16 @@ namespace tello_joy
   constexpr int JOY_AXIS_LEFT_FB = 1;       // Left stick forward/back; 1.0 is forward and -1.0 is back
   constexpr int JOY_AXIS_RIGHT_LR = 3;      // Right stick left/right; 1.0 is left and -1.0 is right
   constexpr int JOY_AXIS_RIGHT_FB = 4;      // Right stick forward/back; 1.0 is forward and -1.0 is back
-  constexpr int JOY_BUTTON_VIEW = 6;        // View button
-  constexpr int JOY_BUTTON_MENU = 7;        // Menu button
+  constexpr int JOY_BUTTON_VIEW = 4;        // View button
+  constexpr int JOY_BUTTON_MENU = 5;        // Menu button
+  constexpr int JOY_BUTTON_X = 0;        // X button
+  constexpr int JOY_BUTTON_CIRCLE = 1;        // Circle button
+  constexpr int JOY_BUTTON_SQUARE = 3;        // Square button
+  constexpr int JOY_BUTTON_TRIANGLE = 2;        // Triangle button
+  constexpr int JOY_BUTTON_UP = 11;     //Up button
+  constexpr int JOY_BUTTON_DOWN = 12;     //Down button
+  constexpr int JOY_BUTTON_LEFT = 6;     //Left button
+  constexpr int JOY_BUTTON_RIGHT = 7;      //Right button
 
   class TelloJoyNode : public rclcpp::Node
   {
@@ -34,6 +43,18 @@ namespace tello_joy
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
     rclcpp::Client<tello_msgs::srv::TelloAction>::SharedPtr tello_client_;
 
+    //Services for 4 main buttons
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr x_client_;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr circle_client_;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr rectangle_client_;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr triangle_client_;
+
+    //Services for 4 direction buttons
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr b_up_client_;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr b_down_client_;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr b_left_client_;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr b_right_client_;
+
     // XBox One assignments
     const int joy_axis_throttle_ = JOY_AXIS_RIGHT_FB;
     const int joy_axis_strafe_ = JOY_AXIS_RIGHT_LR;
@@ -41,6 +62,16 @@ namespace tello_joy
     const int joy_axis_yaw_ = JOY_AXIS_LEFT_LR;
     const int joy_button_takeoff_ = JOY_BUTTON_MENU;
     const int joy_button_land_ = JOY_BUTTON_VIEW;
+
+    const int joy_button_x_ = JOY_BUTTON_X;
+    const int joy_button_circle_ = JOY_BUTTON_CIRCLE;
+    const int joy_button_rectangle_ = JOY_BUTTON_SQUARE;
+    const int joy_button_triangle_ = JOY_BUTTON_TRIANGLE;
+
+    const int joy_button_up_= JOY_BUTTON_UP;
+    const int joy_button_down_= JOY_BUTTON_DOWN;
+    const int joy_button_left_= JOY_BUTTON_LEFT;
+    const int joy_button_right_= JOY_BUTTON_RIGHT;
   };
 
 } // namespace tello_joy
--- tello_driver/src/tello_joy_node.cpp
+++ tello_driver/src/tello_joy_node.cpp
@@ -4,6 +4,7 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 #include "tello_msgs/srv/tello_action.hpp"
+#include "std_srvs/srv/trigger.hpp"
 
 namespace tello_joy
 {
@@ -14,8 +15,18 @@ namespace tello_joy
     using std::placeholders::_1;
 
     joy_sub_ = create_subscription<sensor_msgs::msg::Joy>("joy", 1, std::bind(&TelloJoyNode::joy_callback, this, _1));
-    cmd_vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 1);
+    cmd_vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel_joy", 1);
     tello_client_ = create_client<tello_msgs::srv::TelloAction>("tello_action");
+
+    x_client_ = create_client<std_srvs::srv::Trigger>("x");
+    circle_client_ = create_client<std_srvs::srv::Trigger>("circle");
+    rectangle_client_ = create_client<std_srvs::srv::Trigger>("rectangle");
+    triangle_client_ = create_client<std_srvs::srv::Trigger>("triangle");
+
+    b_up_client_ = create_client<std_srvs::srv::Trigger>("b_up");
+    b_down_client_ = create_client<std_srvs::srv::Trigger>("b_down");
+    b_left_client_ = create_client<std_srvs::srv::Trigger>("b_left");
+    b_right_client_ = create_client<std_srvs::srv::Trigger>("b_right");
 
     (void) joy_sub_;
   }
@@ -34,6 +45,30 @@ namespace tello_joy
       auto request = std::make_shared<tello_msgs::srv::TelloAction::Request>();
       request->cmd = "land";
       tello_client_->async_send_request(request);
+    } else if (joy_msg->buttons[joy_button_x_]) { 
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      x_client_->async_send_request(request);
+    } else if (joy_msg->buttons[joy_button_circle_]) {
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      circle_client_->async_send_request(request);
+    } else if (joy_msg->buttons[joy_button_rectangle_]) {    
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      rectangle_client_->async_send_request(request);
+    } else if (joy_msg->buttons[joy_button_triangle_]) {   
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      triangle_client_->async_send_request(request);
+    } else if (joy_msg -> buttons[joy_button_up_]){
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      b_up_client_->async_send_request(request);
+    } else if (joy_msg -> buttons[joy_button_down_]){
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      b_down_client_->async_send_request(request);
+    } else if (joy_msg -> buttons[joy_button_left_]){
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      b_left_client_->async_send_request(request);
+    } else if (joy_msg -> buttons[joy_button_right_]){
+      auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+      b_right_client_->async_send_request(request);
     } else {
       geometry_msgs::msg::Twist twist_msg;
       twist_msg.linear.x = joy_msg->axes[joy_axis_throttle_];
